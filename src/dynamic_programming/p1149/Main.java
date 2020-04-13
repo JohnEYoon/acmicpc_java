b0a70b6169158668c1360cecd9c8ce58c7b01420
HY <johnhyowon@gmail.com>
2020-04-13 11:12:24 +0900

    1: p1149: (COMPLETE) Dynamic Programming

--- src/dynamic_programming/p1149/Main.java
+++ src/dynamic_programming/p1149/Main.java
@@ -6,39 +6,55 @@ import java.util.StringTokenizer;
 
 public class Main {
 	public static int[][] values;
+	public static long[][] MEMO;
 	public static int N;
 	public static long min = Long.MAX_VALUE;
 	
-	public static void DFS(int index, int color, long val) {
+	public static void DFS(int index) {
 		if(index == N ) {
-			if(val < min) {
-				min = val;
-			}
-		}
-		if(val >= min) {
+			for(int i=1;i<=3;i++) {
+				if(MEMO[index][i]<min) {
+					min = MEMO[index][i];
+				}
+			}	
 			return;
 		}
-		for(int i=0;i<3;i++) {
-			if(i != color) {
-				DFS(index+1, i, val+values[index+1][i]);
-			}
-		}
 		
-		return;
+		for(int i=1;i<=3;i++) {
+			int x1, x2;
+			if(i == 1) {
+				x1 = 2;
+				x2 = 3;
+			}else if(i==2) {
+				x1 = 1;
+				x2 = 3;
+			}else {
+				x1 = 1;
+				x2 = 2;
+			}
+			
+			MEMO[index+1][i] = Math.min(MEMO[index][x1]+values[index+1][i], MEMO[index][x2]+values[index+1][i]);
+		}
+		DFS(index+1);
 	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		values = new int[N+1][3];
+		values = new int[N+1][4];
+		MEMO = new long[N+1][4];
 		StringTokenizer st;
 		for(int i=1;i<=N;i++) {
 			st = new StringTokenizer(br.readLine());
-			for(int j=0;j<3;j++) {
+			for(int j=1;j<=3;j++) {
 				values[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		DFS(0,-1,0);
+		MEMO[1][1] = values[1][1];
+		MEMO[1][2] = values[1][2];
+		MEMO[1][3] = values[1][3];
+		
+		DFS(1);
 		System.out.println(min);
 	}
 
