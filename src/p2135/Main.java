78e1538860dc8fc4a7559d4fe46f2daa6cdba95c
HY <johnhyowon@gmail.com>
2020-04-14 09:13:44 +0900

    p2135 use dyanmic programming still working on it

--- src/p2135/Main.java
+++ src/p2135/Main.java
@@ -4,86 +4,54 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
-	public static boolean compressionCheck(String word) {
-		for(int i=0;i<word.length()-1;i++) {
-			int length = 0;
-			for(int j=i+1;j<word.length();j++) {
-				length = j-i;
-				if(length+j > word.length()) {
+	public static String compress(String word) {
+		int N = word.length()/2;
+		for(int i=1;i<=N;i++) {
+			for(int j=0;j<word.length();j++) {
+				if((j+2*i) > word.length()) {
 					break;
 				}
-				if(word.charAt(i) == word.charAt(j)) {
-					if(word.substring(i, j).equals(word.substring(j, j+length))){
-						String temp = word.substring(i, j);
-						int startIdx = j;
-						int endIdx = j+length;
-						int count = 2;
-						while(endIdx+length<=word.length()) {
-							if(temp.equals(word.substring(startIdx+length, endIdx+length))) {
-								startIdx += length;
-								endIdx += length;
-								count++;
-							}else {
-								break;
-							}
-						}
-						if(word.length() > (word.substring(0, i)+count+"("+temp+")"+word.substring(endIdx)).length()) {
-							return true;
-						}else {
-							return false;
+				
+				if(word.substring(j, j+i).equals(word.substring(j+i, j+2*i))) {
+					int count = 2;
+					String temp = word.substring(j, j+i);
+					boolean safe = false;
+					for(int k=0;k<temp.length();k++) {
+						if(Character.isAlphabetic(temp.charAt(k))) {
+							safe = true;
+							break;
 						}
 					}
-				}
-			}
-			
-		}
-		return false;
-	}
-	
-	public static String RLE(String word) {
-		for(int i=0;i<word.length()-1;i++) {
-			int length = 0;
-			for(int j=i+1;j<word.length();j++) {
-				length = j-i;
-				if(length+j > word.length()) {
-					break;
-				}
-				if(word.charAt(i) == word.charAt(j)) {
-					if(word.substring(i, j).equals(word.substring(j, j+length))){
-						String temp = word.substring(i, j);
-						int startIdx = j;
-						int endIdx = j+length;
-						int count = 2;
-						while(endIdx+length<=word.length()) {
-							if(temp.equals(word.substring(startIdx+length, endIdx+length))) {
-								startIdx += length;
-								endIdx += length;
-								count++;
-							}else {
-								break;
-							}
-						}
-						if(word.length() > (word.substring(0, i)+count+"("+temp+")"+word.substring(endIdx)).length()) {
-							return word.substring(0, i)+count+"("+temp+")"+RLE(word.substring(endIdx));
+					
+					if(!safe) {
+						break;
+					}
+					int beginIndex = j+i;
+					int endIndex = j+2*i;
+					while(endIndex+i<=word.length()) {
+						if(temp.equals(word.substring(beginIndex+i, endIndex+i))) {
+							beginIndex += i;
+							endIndex += i;
+							count++;
 						}else {
-							return word.substring(0, endIdx)+RLE(word.substring(endIdx));
+							break;
 						}
 					}
+					if( (word.substring(0, j)+count+"("+temp+")"+word.substring(endIndex)).length() < word.length() ) {
+						return compress(word.substring(0, j)+count+"("+temp+")"+word.substring(endIndex));
+					}
+					
 				}
 			}
-			
 		}
+		
 		return word;
 	}
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String word = br.readLine();
-		while(compressionCheck(word)) {
-			word = RLE(word);
-		}
-		System.out.println(word);
+		word = compress(word);
 		System.out.println(word.length());
 	}
-
 }
+
